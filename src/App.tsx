51b771290f0ea255198f3d8019e3583ea226422d
gabrielalves12y-art <230257095+gabrielalves12y-art@users.noreply.github.com>
2026-09-18 19:31:57 -0300

    feat: CartaoLivro recebe o livro por props

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import type { Livro } from '../types/entidades';
+import { CartaoLivro } from './componentes/CartaoLivro';
+const domCasmurro: Livro = {
+id: 1,
+titulo: 'Dom Casmurro',
+autor: 'Machado de Assis',
+sinopse: 'Romance narrado por Bentinho.',
+exemplares: 3,
+};
+const vidasSecas: Livro = {
+id: 2,
+titulo: 'Vidas Secas',
+autor: 'Graciliano Ramos',
+exemplares: 1,
+};
 export default function App() {
-return <main><h1>Sistema de Gerenciamento de Pedidos</h1></main>;
+return (
+<main>
+<CartaoLivro livro={domCasmurro} />
+<CartaoLivro livro={vidasSecas} />
+</main>
+);
 }
